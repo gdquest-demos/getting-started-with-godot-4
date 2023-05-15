4eadbb956df5dcfa86a1b70de9e8e3753491b0df
Xananax <me@xananax.com>
2023-05-15 14:28:22 +0200

    make gun shoot continuously, regardless of enemies

--- vampire_survivors/Gun.gd
+++ vampire_survivors/Gun.gd
@@ -5,16 +5,16 @@ extends Area2D
 
 func _process(delta):
 	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range:
+	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
-		if timer.is_stopped():
-			shoot_at(target_enemy)
 
-func shoot_at(target):
-	timer.start()
-
+func shoot():
 	const BULLET = preload("res://vampire_survivors/bullet_2d.tscn")
 	var new_bullet = BULLET.instantiate()
 	new_bullet.global_transform = shooting_point.global_transform
 	shooting_point.add_child(new_bullet)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
