d581741ea32a1f278bf2b12f5b16e67503172d4e
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-06-01 16:10:53 +0200

    make enemies spawn faster over time

--- 3d-project-completed/spawner_3d.gd
+++ 3d-project-completed/spawner_3d.gd
@@ -3,6 +3,11 @@ extends Node3D
 signal mob_spawned(mob)
 
 @export var mob_to_spawn: PackedScene = preload("res://mob.tscn")
+@export var start_time_between_spawn = 4.0
+
+
+func _ready():
+	%Timer.wait_time = start_time_between_spawn
 
 
 func _on_timer_timeout():
@@ -10,3 +15,7 @@ func _on_timer_timeout():
 	add_child(new_mob)
 	new_mob.global_position = global_position
 	mob_spawned.emit(new_mob)
+	
+	# Each time a mob spawns, we make the game a little harder
+	const MINIMUM_SPAWN_TIME = 1.5
+	%Timer.wait_time = max(MINIMUM_SPAWN_TIME, %Timer.wait_time - 0.1)
