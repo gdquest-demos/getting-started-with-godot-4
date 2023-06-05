948366fce8b2b4991dc06369dfd4c2b06319749f
Tibo <goiffonthibaud@protonmail.com>
2023-06-05 10:58:01 +0200

    add: slime animations + effects

--- 2d-project-completed/mob.gd
+++ 2d-project-completed/mob.gd
@@ -5,7 +5,12 @@ var speed = randf_range(200, 300)
 var health = 3
 
 @onready var player := get_node("/root/Game/Player")
+@onready var slime = %Slime
 
+signal KO
+
+func _ready():
+	slime.play_walk()
 
 func _physics_process(delta):
 	if not is_instance_valid(player):
@@ -17,6 +22,8 @@ func _physics_process(delta):
 
 
 func take_damage():
+	slime.play_hurt()
 	health -= 1
 	if health == 0:
+		emit_signal("KO")
 		queue_free()
