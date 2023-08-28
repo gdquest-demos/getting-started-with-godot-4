cb7781895c127b713f27f13788c15cd6921ce6ac
Xananax <me@xananax.com>
2023-08-28 19:13:02 +0200

    Revert "invert controls"
    I thought the little Godot was moving, when it was actually the character.
    Some tilemap or background is necessary, this is very confusing.
    
    This reverts commit fd550b49f7354a04d7dd1720e39b196fb110e4e8.

--- 2d-character-completed/player.gd
+++ 2d-character-completed/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 func _physics_process(delta):
 	const SPEED = 600.0
 	const ACCELERATION = 30
-	var direction = Input.get_vector("move_right", "move_left", "move_back", "move_forward")
+	var direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
 	if (direction != Vector2.ZERO):
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
