138f176942a3de4103f73e65d8b26a7cf8ffa664
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-08-30 14:44:22 +0200

    clean up completed 2d project

--- 2d-project-completed/pistol/impact/impact.gd
+++ 2d-project-completed/pistol/impact/impact.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 
 func _ready():
-	var t = create_tween().set_parallel(true).set_ease(Tween.EASE_OUT)
-	t.tween_property(sprite, "scale", Vector2.ONE * 1.35, 0.3).from(Vector2.ONE * 0.6)
-	t.tween_property(sprite, "modulate:a", 0.0, 0.15).set_delay(0.15)
-	t.chain().tween_callback(queue_free)
+	var tween = create_tween().set_parallel(true).set_ease(Tween.EASE_OUT)
+	tween.tween_property(sprite, "scale", Vector2.ONE * 1.35, 0.3).from(Vector2.ONE * 0.6)
+	tween.tween_property(sprite, "modulate:a", 0.0, 0.15).set_delay(0.15)
+	tween.chain().tween_callback(queue_free)
