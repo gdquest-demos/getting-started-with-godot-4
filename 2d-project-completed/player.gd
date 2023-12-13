c1bda325b562c7e39452cd76f2320cd107418f02
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-12-13 10:58:09 +0100

    chore: clean up comment in player script

--- 2d-project-completed/player.gd
+++ 2d-project-completed/player.gd
@@ -24,6 +24,4 @@ func _physics_process(delta):
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
 		%HealthBar.value = health
 		if health <= 0.0:
-			# consider using queue_free and listening to tree_exited
-			# unless you mean to have an animation / juice
 			health_depleted.emit()
