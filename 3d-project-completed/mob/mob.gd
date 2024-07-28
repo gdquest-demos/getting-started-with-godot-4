0dd0f8b496477e5a608c3618e6a5a2173221b8ec
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-07-28 13:24:08 +0200

    shift line of code in mob script

--- 3d-project-completed/mob/mob.gd
+++ 3d-project-completed/mob/mob.gd
@@ -22,20 +22,27 @@ func _physics_process(delta):
 func take_damage():
 	if health <= 0:
 		return
+
 	bat_skin.hurt()
+	health -= 1
 	hurt_sound.pitch_scale = randfn(1.0, 0.1)
 	hurt_sound.play()
-	health -= 1
+
 	if health == 0:
 		ko_sound.pitch_scale = randfn(1.0, 0.1)
 		ko_sound.play()
+
 		set_physics_process(false)
+
 		gravity_scale = 1.0
+
 		var direction_back = player.global_position.direction_to(global_position)
 		var random_upward_force = Vector3.UP * randf() * 5.0
 		apply_central_impulse(direction_back.rotated(Vector3.UP, randf_range(-0.2, 0.2)) * 10.0 + random_upward_force)
+
 		%DeathTimer.start()
-	
+
+
 func _on_death_timer_timeout():
 	died.emit()
 	queue_free()
