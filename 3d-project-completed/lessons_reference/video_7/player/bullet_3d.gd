35c97520db7c13586ddfef9ce1cb71339440cb8d
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-02-26 15:25:22 +0100

    chore: add completed reference at the end of video 7

---
+++ 3d-project-completed/lessons_reference/video_7/player/bullet_3d.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+const SPEED = 55.0
+const RANGE = 40.0
+
+var travelled_distance = 0.0
+
+
+func _physics_process(delta):
+	position += transform.basis.z * SPEED * delta
+	travelled_distance += SPEED * delta
+	if travelled_distance > RANGE:
+		queue_free()
+
+
+func _on_body_entered(body):
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
---
+++ 3d-project-completed/lessons_reference/video_7/player/player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody3D
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * 0.5
+		%Camera3D.rotation_degrees.x -= event.relative.y * 0.2
+		%Camera3D.rotation_degrees.x = clamp(
+			%Camera3D.rotation_degrees.x, -60.0, 60.0
+		)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _physics_process(delta):
+	const SPEED = 5.5
+
+	var input_direction_2D = Input.get_vector(
+		"move_left", "move_right", "move_forward", "move_back"
+	)
+	var input_direction_3D = Vector3(
+		input_direction_2D.x, 0, input_direction_2D.y
+	)
+	var direction = transform.basis * input_direction_3D
+
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+
+	velocity.y -= 20.0 * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = 10.0
+	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
+		velocity.y = 0.0
+
+	move_and_slide()
+
+	if Input.is_action_pressed("shoot") and %Timer.is_stopped():
+		shoot_bullet()
+
+
+func shoot_bullet():
+	const BULLET_3D = preload("bullet_3d.tscn")
+	var new_bullet = BULLET_3D.instantiate()
+	%Marker3D.add_child(new_bullet)
+
+	new_bullet.transform = %Marker3D.global_transform
+
+	%Timer.start()
